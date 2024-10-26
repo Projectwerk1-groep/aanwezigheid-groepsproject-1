8ef2deaa71fee3e4b58b653e25f56fe067dc2b89
OrlandoMollaert <orlando.mollaert@student.hogent.be>
2024-10-27 00:11:50 +0200

    code commit

---
+++ AanwezigheidProject/Aanwezigheid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AanwezigheidProject
+{
+    public class Aanwezigheid
+    {  
+        public int SpelerID { get; set; }
+        public int TrainingID { get; set; }
+        public bool IsAanwezig { get; set; }
+        public bool HeeftAfwezigheidGemeld { get; set; }
+        public string RedenAfwezigheid { get; set; }
+    }
+}
---
+++ AanwezigheidProject/SpelerException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AanwezigheidProject
+{
+    public class SpelerException : Exception
+    {
+        public SpelerException(string? message) : base(message)
+        {
+        }
+
+        public SpelerException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
